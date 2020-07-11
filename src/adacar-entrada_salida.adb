34a964e98638d014cd143266615a9750403597e0
Asier Zulueta <asierzulueta@live.com>
2020-07-11 16:55:17 +0200

    Se completan las operaciones de Entrada_Salida.

--- src/adacar-entrada_salida.adb
+++ src/adacar-entrada_salida.adb
@@ -2,14 +2,17 @@ with AdaCar.Parametros;
 
 package body AdaCar.Entrada_Salida is
 
+   -----------------------
+   -- Entrada_Salida_PO --
+   -----------------------
 
    protected Entrada_Salida_PO
      with Priority => Parametros.Techo_Entrada_Salida_PO
    is
-      function Init_System return Boolean;
+      procedure Init_System;
       function Lectura_Digital(Canal: Canal_DI) return Estado_Digital;
       procedure Salida_Digital(Canal: Canal_DO; Valor: Estado_Digital);
-      procedure Configura_Analogico (Canal : Canal_AI);
+      procedure Comienza_Analogico (Canal : Canal_AI);
       function Lectura_Analogico (Canal : Canal_AI) return Unidades_AI;
    end Entrada_Salida_PO;
 
@@ -18,9 +21,9 @@ package body AdaCar.Entrada_Salida is
    -- Init_System --
    -----------------
 
-   function Init_System return Boolean is
+   procedure Init_System  is
    begin
-      return Entrada_Salida_PO.Init_System;
+      Entrada_Salida_PO.Init_System;
    end Init_System;
 
    ---------------------
@@ -45,10 +48,10 @@ package body AdaCar.Entrada_Salida is
    -- Configura_Analogico --
    -------------------------
 
-   procedure Configura_Analogico (Canal : Canal_AI) is
+   procedure Comienza_Analogico (Canal : Canal_AI) is
    begin
-      Entrada_Salida_PO.Configura_Analogico(Canal);
-   end Configura_Analogico;
+      Entrada_Salida_PO.Comienza_Analogico(Canal);
+   end Comienza_Analogico;
 
    -----------------------
    -- Lectura_Analogico --
@@ -61,35 +64,70 @@ package body AdaCar.Entrada_Salida is
 
 
 
+   -----------------------
+   -- Entrada_Salida_PO --
+   -----------------------
 
    protected body Entrada_Salida_PO is
 
-      function Init_System return Boolean is
+      procedure Init_System is
       begin
-         null;
-         return True;
+         Digital.Configure_Pin(Pin_D2,Input);
+         Digital.Configure_Pin(Pin_D3,Input);
+         Digital.Configure_Pin(Pin_D4,Input);
+         Digital.Configure_Pin(Pin_D5,Input);
+         Digital.Configure_Pin(Pin_D6,Input);
+         Digital.Configure_Pin(Pin_D7,Input);
+
+         Digital.Configure_Pin(Pin_D8,Output);
+         Digital.Configure_Pin(Pin_D9,Output);
+         Digital.Configure_Pin(Pin_D10,Output);
+         Digital.Configure_Pin(Pin_D11,Output);
+         Digital.Configure_Pin(Pin_D12,Output);
+         Digital.Configure_Pin(Pin_D13,Output);
+
+         Digital.Set_Signal(Pin_D8,LOW);
+         Digital.Set_Signal(Pin_D9,LOW);
+         Digital.Set_Signal(Pin_D10,LOW);
+         Digital.Set_Signal(Pin_D11,LOW);
+         Digital.Set_Signal(Pin_D12,LOW);
+         Digital.Set_Signal(Pin_D13,LOW);
+
+         Analog.Configure_Pin(Pin_A0);
+         Analog.Configure_Pin(Pin_A1);
+         Analog.Configure_Pin(Pin_A2);
       end Init_System;
 
       function Lectura_Digital(Canal: Canal_DI) return Estado_Digital is
+         Valor: Signal_Mode;
       begin
-         null;
-         return Estado_Digital'(0);
+         Valor:= Digital.Read_Signal(Canal);
+         case Valor is
+            when LOW =>
+               return Estado_Digital'(0);
+            when HIGH =>
+               return Estado_Digital'(1);
+         end case;
       end Lectura_Digital;
 
       procedure Salida_Digital(Canal: Canal_DO; Valor: Estado_Digital) is
       begin
-         null;
+         case Valor is
+            when 0 =>
+               Digital.Set_Signal(Canal,LOW);
+            when 1 =>
+               Digital.Set_Signal(Canal,HIGH);
+         end case;
       end Salida_Digital;
 
-      procedure Configura_Analogico (Canal : Canal_AI) is
+      procedure Comienza_Analogico (Canal : Canal_AI) is
       begin
-         null;
-      end Configura_Analogico;
+         Analog.Start_Adquisition(Canal);
+      end Comienza_Analogico;
 
       function Lectura_Analogico (Canal : Canal_AI) return Unidades_AI is
       begin
-         null;
-         return Unidades_AI'(0);
+         return Unidades_AI(Analog.Get_Value(Canal));
       end Lectura_Analogico;
 
    end Entrada_Salida_PO;
--- src/adacar-entrada_salida.ads
+++ src/adacar-entrada_salida.ads
@@ -1,12 +1,16 @@
+with Digital; use Digital;
+with Analog; use Analog;
+with Pins_STM32F446; use Pins_STM32F446;
+
 package AdaCar.Entrada_Salida is
 
-   function Init_System return Boolean;
+   procedure Init_System;
    
    function Lectura_Digital(Canal: Canal_DI) return Estado_Digital;
    
    procedure Salida_Digital(Canal: Canal_DO; Valor: Estado_Digital);
    
-   procedure Configura_Analogico(Canal: Canal_AI);
+   procedure Comienza_Analogico(Canal: Canal_AI);
    
    function Lectura_Analogico(Canal: Canal_AI) return Unidades_AI;
    
