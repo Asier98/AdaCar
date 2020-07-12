61ec21f5388574ae8ca168732edc78cea6e0b844
Asier Zulueta <asierzulueta@live.com>
2020-07-12 14:42:49 +0200

    Se crea una primera versión del paquete Sensor_Proximidad: se completa la función Leer_Entrada_Sensor  y se crea la función Subida_Pulso que devuelve el tiempo en el que la señal de un determinado canal pasa de LOW a HIGH.

--- src/adacar-sensor_proximidad.adb
+++ src/adacar-sensor_proximidad.adb
@@ -1,7 +1,12 @@
 with AdaCar.Parametros;
+with AdaCar.Entrada_Salida;
 
 package body AdaCar.Sensor_Proximidad is
 
+
+   --------------------------
+   -- Sensor_Proximidad_PO --
+   --------------------------
 
    protected Sensor_Proximidad_PO
      with Priority => Parametros.Techo_Sensor_Proximidad_PO
@@ -10,6 +15,40 @@ package body AdaCar.Sensor_Proximidad is
       function Leer_Entrada_Sensor return Unidades_Distancia;
 
    end Sensor_Proximidad_PO;
+
+
+
+
+   function Subida_Pulso(Canal_Entrada: in Canal_DI;
+                          Canal_Salida: in Canal_DO) return Duration
+   is
+      Next: Real_Time.Time:= Real_Time.Clock;
+      Intervalo: constant Real_Time.Time_Span:= Real_Time.To_Time_Span(Duration(0.00001));
+
+      Tiempo_Inicial: Real_Time.Time;
+      Tiempo_Final: Real_Time.Time;
+      Valor_Senhal: Estado_Digital;
+      Dif: Duration;
+   begin
+      Next:= Real_Time.Clock;
+      Entrada_Salida.Salida_Digital(Canal_Salida,Estado_Digital'(1));
+      Tiempo_Inicial:= Real_Time.Clock;
+      Next:= Next+Intervalo;
+      delay until Next;
+      Entrada_Salida.Salida_Digital(Canal_Salida,Estado_Digital'(0));
+
+      loop
+         Valor_Senhal:= Entrada_Salida.Lectura_Digital(Canal_Entrada);
+         exit when Valor_Senhal=Estado_Digital'(1);
+      end loop;
+
+      Tiempo_Final:= Real_Time.Clock;
+      Dif:= Real_Time.To_Duration(Tiempo_Final-Tiempo_Inicial);
+
+      return  Dif;
+   end Subida_Pulso;
+
+
 
 
    -------------------------
@@ -22,12 +61,24 @@ package body AdaCar.Sensor_Proximidad is
    end Leer_Entrada_Sensor;
 
 
-    protected body Sensor_Proximidad_PO is
+
+   --------------------------
+   -- Sensor_Proximidad_PO --
+   --------------------------
+
+   protected body Sensor_Proximidad_PO is
 
       function Leer_Entrada_Sensor return Unidades_Distancia is
+         Duracion_Distancia: Duration;
+         Distancia: Unidades_Distancia;
       begin
-         null;
-         return Parametros.Convertidor_Distancia(Unidades_AI(0));
+
+         Duracion_Distancia:= Subida_Pulso(Canal_Entrada => Parametros.Canal_DI_Echo,
+                                           Canal_Salida  => Parametros.Canal_DO_Trig);
+
+         Distancia:= Parametros.Factor_Distancia*Duracion_Distancia;
+
+         return Distancia;
       end Leer_Entrada_Sensor;
 
 
