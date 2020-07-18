3a8951e88f6246ed3644e88fadf62424c4a956cf
Asier Zulueta <asierzulueta@live.com>
2020-07-18 18:20:15 +0200

    Se declaran los canales empleados para el manejo de los motores.

--- src/adacar-parametros.ads
+++ src/adacar-parametros.ads
@@ -22,6 +22,17 @@ package AdaCar.Parametros is
    Canal_DO_Trig: Canal_DO:= Pin_D8;
    Canal_DI_Echo: Canal_DI:= Pin_D6;
      
+   -- Se han atribuido canales aleatorios. Se tienen que configurar más adelante.
+   Canal_DO_Motor_Derecho_A: Canal_DO:= Pin_D2;
+   Canal_DO_Motor_Derecho_B: Canal_DO:= Pin_D3;
+   Canal_DO_Motor_Derecho_C: Canal_DO:= Pin_D4;
+   Canal_DO_Motor_Derecho_D: Canal_DO:= Pin_D5;
+   
+   Canal_DO_Motor_Izquierdo_A: Canal_DO:= Pin_D10;
+   Canal_DO_Motor_Izquierdo_B: Canal_DO:= Pin_D11;
+   Canal_DO_Motor_Izquierdo_C: Canal_DO:= Pin_D12;
+   Canal_DO_Motor_Izquierdo_D: Canal_DO:= Pin_D13;
+     
    
    
    --------------
