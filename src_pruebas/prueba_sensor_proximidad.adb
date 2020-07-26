3936f37817020c5da3508539ae7ca417d6d645c1
Asier Zulueta <asierzulueta@live.com>
2020-07-26 16:21:05 +0200

    Se añade una explicación del main.

--- src_pruebas/prueba_sensor_proximidad.adb
+++ src_pruebas/prueba_sensor_proximidad.adb
@@ -4,6 +4,10 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+-- Se emite una señal digital por el canal 'Pin_Distancia' cuando el sensor de
+-- proximidad detecta una distancia inferior a la distancia 'Distancia_Analisis'
+-- establecida. No emite señal si la distancia es mayor.
+
 with AdaCar; use AdaCar;
 with Pins_STM32F446; use Pins_STM32F446;
 with Ada.Real_Time; use type Ada.Real_Time.Time; use Ada;
@@ -14,6 +18,7 @@ procedure Prueba_Sensor_Proximidad is
    Pines_Activados: Boolean:= False;
    Distancia: Unidades_Distancia;
    Pin_Distancia: constant Canal_DO:= Pin_D13;
+   Distancia_Analisis: constant Unidades_Distancia:= Unidades_Distancia(50.0);
 
    Tiempo: constant Duration:= 0.1;
    Periodo: constant Real_Time.Time_Span:= Real_Time.To_Time_Span(Tiempo);
@@ -25,7 +30,7 @@ begin
    if Pines_Activados then
       loop
          Distancia:= Sensor_Proximidad.Leer_Entrada_Sensor;
-         if Distancia>50.0 then
+         if Distancia > Distancia_Analisis then
             Entrada_Salida.Salida_Digital(Pin_Distancia,Estado_Digital'(0));
          else
             Entrada_Salida.Salida_Digital(Pin_Distancia,Estado_Digital'(1));
